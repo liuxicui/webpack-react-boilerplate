83dcb9cc81a8d7075c3bacb725aefa9ec491dc30
liuxicui <870566031@qq.com>
2017-03-19 09:05:34 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 
-import Header from './components/Header';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends React.Component {
   render () {
     return(
       <div>
-        <MuiThemeProvider>
-          <Header />
-        </MuiThemeProvider>
       </div>
     )
   }
